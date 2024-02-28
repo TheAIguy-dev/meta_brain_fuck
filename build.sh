d6755e390133d47c7de4dd6145326656eeb951c9
TheAIguy_ <90064221+TheAIguy-dev@users.noreply.github.com>
2024-02-28 10:54:47 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,2 @@
-cross build --target x86_64-pc-windows-gnu --profile release
-upx --best "./target/x86_64-pc-windows-gnu/release/meta_brain_fuck.exe"
-cargo build --target x86_64-unknown-linux-gnu --profile release
-upx --best "./target/x86_64-unknown-linux-gnu/release/meta_brain_fuck"
+cargo build --profile release
+upx --best "./target/release/meta_brain_fuck"
